90066e0a1bae4d97cc327115a779194912c5cef1
codelocker9 <131526840+codelocker9@users.noreply.github.com>
2024-03-29 17:12:00 +0000

    eighth_commit

--- static/authenticate.js
+++ static/authenticate.js
@@ -1,12 +1,12 @@
-const key = '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';
+// const key = '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';
 
-fetch('https://huggingface.co/HuggingFaceH4/zephyr-7b-beta').then(Response => {
-    if (!Response.ok) {
-        throw new Error('Network response was not ok');
-    }
-    return Response.json();
-}).then(data => {
-    console.log(data);
-}).catch(error => {
-    console.error('There has been a problem with your fetch operation: ', error);
-});
+// fetch('https://huggingface.co/HuggingFaceH4/zephyr-7b-beta').then(Response => {
+//     if (!Response.ok) {
+//         throw new Error('Network response was not ok');
+//     }
+//     return Response.json();
+// }).then(data => {
+//     console.log(data);
+// }).catch(error => {
+//     console.error('There has been a problem with your fetch operation: ', error);
+// });
