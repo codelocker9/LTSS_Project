01562ed076e2cdd20497e013a3e49da177a5d3a0
codelocker9 <131526840+codelocker9@users.noreply.github.com>
2024-03-29 12:46:08 +0000

    second_commit

--- static/script.js
+++ static/script.js
@@ -1,32 +1,13 @@
 import { pipeline, env } from "https://cdn.jsdelivr.net/npm/@xenova/transformers@2.6.0";
 
 env.allowLocalModels = false;
+env.HUGGINGFACEHUB_API_TOKEN = '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';
 
-const fileUpload = document.getElementById("file-upload");
-const imageContainer = document.getElementById("image-container");
+const input = document.getElementById("text-input");
+const send_btn = document.getElementById("send-input");
 const status = document.getElementById("status");
 
 // Creating an object detection pipeline
 status.textContent = "Loading model...";
-const detector = await pipeline("object-detection", "Xenova/detr-resnet-50");
+const model_one = await pipeline("text-generation", "google/gemma-7b-it");
 status.textContent = "Ready";
-
-// Image Container
-fileUpload.addEventListener("change", function (e) {
-    const file = e.target.files[0];
-    if (!file) {
-      return;
-    }
-  
-    const reader = new FileReader();
-  
-    // Set up a callback when the file is loaded
-    reader.onload = function (e2) {
-      imageContainer.innerHTML = "";
-      const image = document.createElement("img");
-      image.src = e2.target.result;
-      imageContainer.appendChild(image);
-      // detect(image); // Uncomment this line to run the model
-    };
-    reader.readAsDataURL(file);
-});
