7aaa132e3a0d7fa7d88c9ff9ca8a855a02e0c8b4
codelocker9 <131526840+codelocker9@users.noreply.github.com>
2024-03-30 11:43:15 +0000

    twelve_commit

--- static/script.js
+++ static/script.js
@@ -5,6 +5,7 @@ env.allowLocalModels = false;
 const input = document.getElementById("text-input");
 const send_btn = document.getElementById("send-input");
 const status = document.getElementById("status");
+// const newElement = document.createElement("p"); // Or any other element
 
 // Loading the model
 status.textContent = "Loading model...";
@@ -12,16 +13,10 @@ const model_one = await pipeline("text-generation");
 status.textContent = "Ready";
 
 send_btn.addEventListener('click', async (event) => {
-    // status.textContent = await model_one(input);
     const userInput = input.value;
-
     const escapedUserInput = encodeURIComponent(userInput); // Escape user input here
     status.textContent = "Generating text...";
     const outputText = await model_one(escapedUserInput);
-    const textNode = document.createTextNode(outputText);
-    const newElement = document.createElement("p"); // Or any other element
-    newElement.appendChild(textNode);
-    console.log(outputText);
-    status.innerHTML = ""; // Clear any previous content
-    status.appendChild(newElement);
+    const textNode = outputText[0].generated_text;
+    status.textContent = textNode;
 });
