8b98a15851a8b400332baa04cf20193ee97a887d
codelocker9 <131526840+codelocker9@users.noreply.github.com>
2024-03-31 14:45:06 +0000

    fourteenth_commit

--- static/test.js
+++
@@ -1,6 +0,0 @@
-const { pipeline } = require("transformers");
-// Assuming you've included the Transformers library
-
-const question = "What is the capital of France?";
-const answer = await pipeline("question-answering")(question, "facebook/bart-base-squad2");
-console.log("Answer:", answer.answer);
