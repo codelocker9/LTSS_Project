1910bed90295d0cee17aa678d782d474fac02014
codelocker9 <131526840+codelocker9@users.noreply.github.com>
2024-04-01 16:40:26 +0000

    twentieth_commit

--- static/script.js
+++
@@ -1,7 +0,0 @@
-import { HfInference } from "@Huggingface/Inference"
-import dotenv from "dotenv";
-
-dotenv.config();
-
-const HF_ACCESS_TOKEN = "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";
-const infe
