99d996b17e649bcc8e04c65791aaa6a67076fb50
codelocker9 <131526840+codelocker9@users.noreply.github.com>
2024-04-02 05:01:00 +0000

    twenty_first_commit

---
+++ static/script.js
@@ -0,0 +1,22 @@
+import { pipeline, env } from "https://cdn.jsdelivr.net/npm/@xenova/transformers@2.6.0";
+env.allowLocalModels = false;
+env.HUGGINGFACEHUB_API_KEY = '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';
+
+const input = document.getElementById("text-input");
+const send_btn = document.getElementById("send-input");
+const status = document.getElementById("status");
+
+// Loading the model
+status.textContent = "Loading model...";
+const model_one = await pipeline("text-generation");
+status.textContent = "Ready";
+
+send_btn.addEventListener('click', async (event) => {
+    const userInput = input.value;
+    const escapedUserInput = encodeURIComponent(userInput); // Escape user input here
+    status.textContent = "Generating text...";
+    const outputText = await model_one(escapedUserInput);
+    console.log(outputText);
+    // const textNode = consolel;
+    // status.textContent = textNode;
+});
